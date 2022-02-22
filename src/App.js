eefe1565e937b9b65cc1cbeaefe21a4463f5a899
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-02-22 21:28:34 +0530

    some more exampl sample

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState("");
 
   let increment = () => {
     setCounter(counter + 1);
   };
 
+  const getValue = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div>
-      {counter}
-      <button onClick={increment}>Increment</button>
+      <div>
+        {counter}
+        <button onClick={increment}>Increment</button>
+      </div>
+      <br />
+      <input placeholder="say something..." onChange={getValue} />
+      {value}
     </div>
   );
 }
