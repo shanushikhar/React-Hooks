fb427b3f989719c5b5998e6c99b2cd72b731d53d
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-02-23 09:16:05 +0530

    added some async call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-import UseRef from "./UseRef";
-import UseEffect from "./UseEffect";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function App() {
-  return <UseEffect />;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      //console.log("hi ", res.data);
+      setData(res.data);
+    });
+  }, []);
+
+  return <div>{data && data[0].name}</div>;
 }
