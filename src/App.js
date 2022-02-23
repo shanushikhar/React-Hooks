c8b3b6e30c5124fb19f08083a2974983b71e651b
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-02-23 08:46:47 +0530

    added useReducer hooks with example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + 1 };
+
+    case "toogleValue":
+      return { count: state.count, showText: !state.showText };
+
+    default:
+      return state;
+  }
+};
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  let increment = () => {
-    setCounter(counter + 1);
-  };
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    showText: true,
+  });
 
   return (
     <div>
-      {counter}
-      <button onClick={increment}>Increment</button>
+      <h1>{state.count}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "INCREMENT" });
+          dispatch({ type: "toogleValue" });
+        }}
+      >
+        Click me
+      </button>
+      <div>{state.showText && <p>I am EVEN :)</p>}</div>
     </div>
   );
 }
