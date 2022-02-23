dc3958ecd4bbd9153374f8029c17eddc06cbc64d
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-02-23 09:37:59 +0530

    added another example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
-import UseRef from "./UseRef";
-import UseEffect from "./UseEffect";
+import React, { useRef } from "react";
 
 export default function App() {
-  return <UseEffect />;
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    // inputRef.current.placeholder = "hoho";
+    // inputRef.current.focus(); // to Focus
+    inputRef.current.value = ""; // to clear the field value
+  };
+
+  return (
+    <div>
+      <h1>useRef</h1>
+      <input ref={inputRef} type="text" placeholder="say something..." />
+      <button onClick={onClick}>Change Name / focus input field</button>
+    </div>
+  );
 }
