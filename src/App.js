c94a2c59e0e976787a235e97281f8be8e85ccd34
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-02-24 15:32:32 +0530

    added useMemo example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React, { useRef, useState, createContext } from "react";
-import Login from "./Login";
-import User from "./User";
-
-export const AppContext = createContext(null);
+import React, { useEffect, useState, useMemo } from "react";
+import axios from "axios";
 
 export default function App() {
-  const [username, setUserName] = useState(null);
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => setData(res.data));
+  }, []);
+
+  const getlargestComment = (comment) => {
+    if (!comment) return null;
+    console.log(comment);
+    let largestComment = "";
+    for (let x of comment) {
+      // x.body = largestComment
+      if (x.body.length > largestComment.length) {
+        largestComment = x.body;
+      }
+    }
+    console.log("called.....");
+    return largestComment;
+  };
+
+  const magicMemo = useMemo(() => getlargestComment(data), [data]);
+  // const magicMemo = useMemo(() => {
+  //   return getlargestComment(data);
+  // }, [data]);
+
   return (
-    <AppContext.Provider value={{ username, setUserName }}>
-      <Login />
-      <User />
-    </AppContext.Provider>
+    <div>
+      <h1>useMemo</h1>
+      <div>{magicMemo}</div>
+      <div>
+        <button onClick={() => setToggle(!toggle)}>click me</button>
+        {toggle && <span>you can see me now</span>}
+      </div>
+    </div>
   );
 }
